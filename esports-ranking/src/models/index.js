492dcdf1a09b1f3b3d396fe6df50616b2790323c
VanTu04 <141947683+VanTu04@users.noreply.github.com>
2025-11-12 14:35:00 +0700

    a

--- esports-ranking/src/models/index.js
+++ esports-ranking/src/models/index.js
@@ -5,23 +5,24 @@ import defineUser from './User.js';
 import defineOtp from './Otp.js';
 import defineTeam from './Team.js';
 import defineGame from './Game.js';
+import defineSeason from './Season.js';
 
 // Khởi tạo models
 const User = defineUser(sequelize, DataTypes);
 const user_otp = defineOtp(sequelize, DataTypes);
 const Team = defineTeam(sequelize, DataTypes);
 const Game = defineGame(sequelize, DataTypes);
+const Season = defineSeason(sequelize, DataTypes);
 
-// Thiết lập quan hệ
 
-export { sequelize, Sequelize, User, user_otp, Game, Team };
+export { sequelize, Sequelize, User, user_otp, Game, Team, Season };
 
-//Thêm dòng này nếu bạn muốn import models như 1 object
 export default {
   sequelize,
   Sequelize,
   User,
   user_otp,
   Team,
-  Game
+  Game,
+  Season
 };
--- esports-ranking/src/routes/index.js
+++ esports-ranking/src/routes/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import usersRouter from './user.route.js';
 import gamesRouter from './game.route.js';
 import teamsRouter from './team.route.js';
+import seasonRouter from './season.route.js';
 
 const router = express.Router();
 
 router.use('/users', usersRouter);
 router.use('/games', gamesRouter);
 router.use('/teams', teamsRouter);
+router.use('/seasons', seasonRouter);
 
 export default router;
